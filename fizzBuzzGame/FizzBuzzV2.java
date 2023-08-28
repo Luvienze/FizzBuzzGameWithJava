39580d5b32e3346c7417d2616f1d93599a6fcbf1
Luvienze <94405625+Luvienze@users.noreply.github.com>
2023-08-28 13:29:51 +0300

    Update FizzBuzzV2.java

--- fizzBuzzGame/FizzBuzzV2.java
+++ fizzBuzzGame/FizzBuzzV2.java
@@ -2,17 +2,24 @@ package fizzBuzzGame;
 
 public class FizzBuzzV2 {
 	String game;
+	int sizeOfGame;
 	Player[] players;
 	Player answer;
 	
 	void setGame (String game) {
 		this.game = game;
 	}
+	void setSizeOfGame(int sizeOfGame) {
+		this.sizeOfGame = sizeOfGame;
+	}
 	void setPlayers(Player[] players) {
 		this.players = players;
 	}
-	
-	//void getAnswer (Player answer) {
-		
-	//}	
+	void getRules() {
+		System.out.println("      RULES OF THE GAME      ");
+		System.out.println(" Game can be playable by 3 person. ");
+		System.out.println(" Number divisible by three with word is 'fizz' .");
+		System.out.println(" Number divisible by five with word is 'buzz' .");
+		System.out.println(" Number divisible both by five and three with word is 'fizz buzz'. ");
+	}
 }
